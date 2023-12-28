e0bc9fec77a4dd0dc0d5c6fb77a1224ef1b4bc93
dibya888 <dibya.dhar@northsouth.edu>
2023-12-29 01:17:12 +0600

    V0.2 Selenium Assignment 2 initial commit

---
+++ seleniumassignment2/src/test/java/trytestingthis/Test.java
@@ -0,0 +1,65 @@
+package trytestingthis;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test {
+
+	public static void main(String[] args) throws InterruptedException{
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.firefoxdriver();
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://trytestingthis.netlify.app/");
+		driver.manage().window().maximize();
+		
+		//Name
+		WebElement firstname = driver.findElement(By.id("fname"));
+		firstname.sendKeys("Dibya Jyoti");
+		WebElement lastname = driver.findElement(By.id("lname"));
+		lastname.sendKeys("Dhar");
+		
+		//Radio Button
+		WebElement radioButton = driver.findElement(By.id("male"));
+		radioButton.click();
+		
+		//Drop down
+		WebElement testdropdown = driver.findElement(By.id("option"));
+		Select dropdown = new Select(testdropdown);
+				
+		dropdown.selectByVisibleText("Option 2");
+		
+		//Scroll
+		WebElement testscroll = driver.findElement(By.id("owc"));
+		Select scroll = new Select(testscroll);
+		
+		scroll.selectByVisibleText("Option 3");	
+		
+		//Checklist
+		WebElement checklist1 = driver.findElement(By.name("option1"));
+		checklist1.click();
+		WebElement checklist2 = driver.findElement(By.name("option2"));
+		checklist2.click();
+		WebElement checklist3 = driver.findElement(By.name("option3"));
+		checklist3.click();
+		
+		//Long Message
+		WebElement longmsg = driver.findElement(By.name("message"));
+		longmsg.clear();
+		longmsg.sendKeys("Hello, I'm Dibya. I want to build my career on Software Quality Assurance.");
+		
+		//Submit
+		WebElement submit = driver.findElement(By.className("btn-success"));
+		Thread.sleep(10000);
+		submit.click();
+		
+
+	}
+
+}
